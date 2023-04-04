7d46af7de3d425acbd2dc94bbceb21299db88fcc
Soniya Seervi <soniyaseervi12@gmail.com>
2023-04-04 16:38:26 +0530

    Added category wise news display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
-
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-import NewsItem from './components/NewsItem';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 8;
   render() {
     return (
       <div>
+        <Router>
         <Navbar></Navbar>
-        <News></News>
+        <Routes>
+          <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country = 'in' category='general'></News>} />
+          <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country = 'in' category='business'></News>} />
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country = 'in' category='entertainment'></News>} />
+          <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country = 'in' category='general'></News>} />
+          <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country = 'in' category='health'></News>} />
+          <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country = 'in' category='science'></News>} />
+          <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country = 'in' category='sports'></News>} />
+          <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country = 'in' category='technology'></News>} />
+        </Routes>
+        
+        
+        </Router>      
       </div>
     )
   }
