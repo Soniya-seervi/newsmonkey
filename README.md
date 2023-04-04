316f0ff2318db0e9e3b70d68c59b3f81b876fbe8
Soniya Seervi <soniyaseervi12@gmail.com>
2023-04-04 17:21:59 +0530

    Infinite Scroll bar

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ### React components lifecycle Diagaram - 
 https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+
+For the infinite scroll component, we have used the react-infinite-scroll-component - https://www.npmjs.com/package/react-infinite-scroll-component
--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export class News extends Component {
 
@@ -27,7 +28,8 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      totalResults: 0
     }
 
     document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey` ;
@@ -40,37 +42,25 @@ export class News extends Component {
        let parsedData = await data.json();
        console.log(parsedData);
        this.setState({
-         page: this.state.page,
          articles: parsedData.articles,
-         loading: false
+         loading: false,
+         totalResults: parsedData.totalResults
        })
   }
 
   async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b94f9e30c81b41938bc8b32ef7137241&page=1&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults
-    })
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b94f9e30c81b41938bc8b32ef7137241&page=1&pageSize=${this.props.pageSize}`;
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    // console.log(parsedData);
+    // this.setState({
+    //   articles: parsedData.articles,
+    //   totalResults: parsedData.totalResults
+    // })
+    this.updateNews()
   }
 
   handlePreviousClick = async () => {
-
-      /* let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b94f9e30c81b41938bc8b32ef7137241&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-       this.setState({loading:true})
-       let data = await fetch(url);
-       let parsedData = await data.json();
-       console.log(parsedData);
-       this.setState({
-         page: this.state.page-1,
-         articles: parsedData.articles,
-         loading: false
-       })
-    */
-
     this.setState({
       page: this.state.page -1
     })
@@ -78,35 +68,44 @@ export class News extends Component {
   }
 
   handleNextClick = async () => {
-
-    /* 
-    if(!(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)))
-    {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b94f9e30c81b41938bc8b32ef7137241&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-      this.setState({loading:true})
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        page: this.state.page+1,
-        articles: parsedData.articles,
-        loading: false
-      })
-    }
-    */
     this.setState({
       page: this.state.page + 1
     })
-    this.updateNews();
-
+    this.updateNews()
   }
+
+    // The fucntion for infinite scroll that is used to fetch more data and concat it to the end results.
+   /* 
+   fetchMoreData = async() => {
+    this.setState({page: this.state.page+1})
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b94f9e30c81b41938bc8b32ef7137241&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+       this.setState({loading:true})
+       let data = await fetch(url);
+       let parsedData = await data.json();
+      //  console.log(parsedData);
+       this.setState({
+         articles: this.state.articles.concat(parsedData.articles),
+         loading: false,
+         totalResults: parsedData.totalResults
+       })
+  };
+  */
 
   render() {
     console.log("rendor")
     return (
-      <div className = "container my-3">
+      <div>
         <h1 className='text-center' style={{margin: '35px 0px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
-        {this.state.loading && <Spinner/>}
-        <div className = "row">
+         {this.state.loading && <Spinner/>} 
+        
+         {/* <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        > */}
+        <div className="container">
+        < div className = "row">
             {!this.state.loading && this.state.articles.map((element) => {
                 return <div className = "col-md-3" key = {element.url}>
                 <NewsItem  title ={element.title? element.title.slice(0,45): " "} description = {element.description? element.description.slice(0,88): " "} 
@@ -119,10 +118,15 @@ export class News extends Component {
             </div> 
             })}  
         </div>
+        </div>
+        {/* </InfiniteScroll> */}
+
         <div className="container d-flex justify-content-between">
         <button disabled={this.state.page <=1} type="button" className="btn btn-dark " onClick={this.handlePreviousClick}> &larr; Previous</button>
         <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick = {this.handleNextClick}>Next &rarr;</button>
         </div>
+
+
       </div>
     )
   }
